7b7757031819b0439a492ed7f68dfd69fa4f4136
CabralWellington <wellington_oliveira@live.com>
2017-09-10 18:44:18 -0400

    nova estrutura

--- index.php
+++ index.php
@@ -1,19 +1,47 @@
+<?php require_once 'config.php'; ?>
+<?php require_once DBAPI; ?>
 
-<!-- Inicializando -->
+<?php include(HEADER_TEMPLATE); ?>
+<?php $db = open_database(); ?>
 
-<?php
-include("pagina/cabecalho.php")
-?>
+<h1>Dashboard</h1>
+<hr />
 
+<?php if ($db) : ?>
 
+<div class="row">
+	<div class="col-xs-6 col-sm-3 col-md-2">
+		<a href="customers/add.php" class="btn btn-primary">
+			<div class="row">
+				<div class="col-xs-12 text-center">
+					<i class="fa fa-plus fa-5x"></i>
+				</div>
+				<div class="col-xs-12 text-center">
+					<p>Novo Cliente</p>
+				</div>
+			</div>
+		</a>
+	</div>
 
+	<div class="col-xs-6 col-sm-3 col-md-2">
+		<a href="customers" class="btn btn-default">
+			<div class="row">
+				<div class="col-xs-12 text-center">
+					<i class="fa fa-user fa-5x"></i>
+				</div>
+				<div class="col-xs-12 text-center">
+					<p>Clientes</p>
+				</div>
+			</div>
+		</a>
+	</div>
+</div>
 
+<?php else : ?>
+	<div class="alert alert-danger" role="alert">
+		<p><strong>ERRO:</strong> Não foi possível Conectar ao Banco de Dados!</p>
+	</div>
 
+<?php endif; ?>
 
-
-
-<!-- Finalizando a pagina -->
-
-<?php
-include("pagina/rodape.php")
-?>
+<?php include(FOOTER_TEMPLATE); ?>

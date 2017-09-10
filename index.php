70f46011f8d025cae9c4b2f5eb3dcceaf4bb1690
CabralWellington <wellington_oliveira@live.com>
2017-09-10 19:35:20 -0400

    nova estrutura

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 <?php require_once DBAPI; ?>
 
 <?php include(HEADER_TEMPLATE); ?>
+<?php $db = open_database(); ?>
 
 <h1>Dashboard</h1>
 <hr />
 
-
+<?php if ($db) : ?>
 
 <div class="row">
 	<div class="col-xs-6 col-sm-3 col-md-2">
@@ -36,6 +37,11 @@
 	</div>
 </div>
 
+<?php else : ?>
+	<div class="alert alert-danger" role="alert">
+		<p><strong>ERRO:</strong> Não foi possível Conectar ao Banco de Dados!</p>
+	</div>
 
+<?php endif; ?>
 
 <?php include(FOOTER_TEMPLATE); ?>
